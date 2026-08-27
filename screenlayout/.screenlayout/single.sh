bb36b2e0a9d074dfad7470928920342e56e1de88
Max Rudra <msivyhin@gmail.com>
2026-08-27 17:36:54 +0300

    fix(qtile): make monitor layout switching deterministic

--- screenlayout/.screenlayout/single.sh
+++ screenlayout/.screenlayout/single.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Single monitor mode: disable external, reset framebuffer/panning to internal native.
 xrandr --output DP-3 --off \
-  --output eDP-1-1 --mode 3840x2160 --primary --dpi 192
+  --output eDP-1-1 --mode 3840x2160 --primary --dpi 192 \
+  --fb 3840x2160 --panning 0x0
